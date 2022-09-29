d69e104477f26e9f30713d4b9a54fc4fc2706645
Zeeshan Ghazanfar <bsef17a502@pucit.edu.pk>
2022-09-30 04:54:51 +0500

    changes

--- app.js
+++ app.js
@@ -71,10 +71,8 @@ app.get('/network_status', (req, res) => {
             console.log(dns)
             console.log(dns2)
             res.status(200).send({ static: 1, ip_address: ip_address, subnet: subnet, gateway: gateway, dns: dns, dns2: dns2 })
-        } else {
-            console.log("sending dhcp response")
-            res.status(200).send("")
         }
+        res.status(200).send({ static: 0 })
     });
 })
 
