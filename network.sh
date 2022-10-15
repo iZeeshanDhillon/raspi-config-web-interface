77ddff78fb5ced74e6b8b8602b2a43d3c42b6d06
Zeeshan Ghazanfar <bsef17a502@pucit.edu.pk>
2022-10-15 22:25:36 +0500

    Changes

--- network.sh
+++ network.sh
@@ -15,13 +15,13 @@ done < /etc/dhcpcd.conf
 if [[ -z $ip_address ]] || [[ -z $router ]] || [[ -z $dns1 ]]; then
     echo "DHCP"
 else
-ip_address=$(echo $ip_address | cut -d '=' -f 2)
-router=$(echo $router | cut -d '=' -f 2)
-dns1=$(echo $dns1 | cut -d '=' -f 2)
-subnet=$(echo $ip_address | cut -d '/' -f 2)
-ip_address2=$(echo $ip_address | cut -d '/' -f 1)
-dns2=$(echo $dns1 | cut -d ' ' -f 2)
-dns1=$(echo $dns1 | cut -d ' ' -f 1)
-subnet=$(python3 -c "import ipaddress;netmask=ipaddress.IPv4Address(0xffffffff << (32 - $subnet) & 0xffffffff);print(netmask)")
-echo "$ip_address2;$subnet;$router;$dns1;$dns2"
+    ip_address=$(echo $ip_address | cut -d '=' -f 2)
+    router=$(echo $router | cut -d '=' -f 2)
+    dns1=$(echo $dns1 | cut -d '=' -f 2)
+    subnet=$(echo $ip_address | cut -d '/' -f 2)
+    ip_address2=$(echo $ip_address | cut -d '/' -f 1)
+    dns2=$(echo $dns1 | cut -d ' ' -f 2)
+    dns1=$(echo $dns1 | cut -d ' ' -f 1)
+    subnet=$(python3 -c "import ipaddress;netmask=ipaddress.IPv4Address(0xffffffff << (32 - $subnet) & 0xffffffff);print(netmask)")
+    echo "$ip_address2;$subnet;$router;$dns1;$dns2"
 fi
